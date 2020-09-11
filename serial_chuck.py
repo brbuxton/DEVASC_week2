a2d1c492866c9b2f1e7ed95e68dd4df114893e22
brbuxton <brbuxton@cisco.com>
2020-09-11 13:53:13 -0400

    completed

--- serial_chuck.py
+++ serial_chuck.py
@@ -5,15 +5,17 @@ import requests
 
 url = 'https://api.chucknorris.io/jokes/random'
 
+
 def get_chuck_joke(api):
     """
     Get a random joke from the Chuck Norris API and return the serial JSON
     :param api:
     :return:serial JSON
     """
-    response = #  TASK 1 - use requests to get a joke.
-    return #  TASK 2 - return the response in JSON format
+    response = requests.get(api)  # TASK 1 - use requests to get a joke.
+    return response.content  # TASK 2 - return the response in JSON format
+
 
 if __name__ == '__main__':
-    joke = json.something(get_chuck_joke(url)) #  TASK 3 - correct the json statement
-    print() #  TASK 4 - print just a joke without any superfluous symbols
+    joke = json.loads(get_chuck_joke(url))  # TASK 3 - correct the json statement
+    print(joke['value'])  # TASK 4 - print just a joke without any superfluous symbols
